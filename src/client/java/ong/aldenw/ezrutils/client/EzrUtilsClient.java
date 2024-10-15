edc0df3fb35ca4d2e2d01e3becd9a9495d02d4da
Alden <31806638+aldnwong@users.noreply.github.com>
2024-10-15 14:30:44 -0700

    initial commit

---
+++ src/client/java/ong/aldenw/ezrutils/client/EzrUtilsClient.java
@@ -0,0 +1,11 @@
+package ong.aldenw.ezrutils.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EzrUtilsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/ong/aldenw/ezrutils/EzrUtils.java
@@ -0,0 +1,15 @@
+package ong.aldenw.ezrutils;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.text.Text;
+import ong.aldenw.ezrutils.formats.RgbInt;
+
+public class EzrUtils implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        
+    }
+}
